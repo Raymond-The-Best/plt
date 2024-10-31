ac3cd58f5c50a3ef1b375e0e794318567a88b096
Raymond-The-Best <user.squire@gmail.com>
2024-10-31 16:59:10 +0200

    US2 - Test 1

--- piglatin.py
+++ piglatin.py
@@ -2,7 +2,7 @@
 class PigLatin:
 
     def __init__(self, phrase: str):
-        self.phrase = phrase
+        self.phrase = phrase if phrase else "nil"
 
     def get_phrase(self) -> str:
         return self.phrase
--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -1,9 +1,14 @@
 import unittest
+
+from transformers.tools.evaluate_agent import translator
+
 from piglatin import PigLatin
 from error import PigLatinError
 
 
 class TestPigLatin(unittest.TestCase):
+
+    # ========= US 1 =========
 
     def test_create_translator(self):
         translator = PigLatin("hello world")
@@ -12,3 +17,9 @@ class TestPigLatin(unittest.TestCase):
     def test_create_translator_and_get_phrase(self):
         translator = PigLatin("hello world")
         self.assertEqual(translator.get_phrase(), "hello world")
+
+    # ========= US 2 =========
+
+    def test_create_translator_empty_phrase(self):
+        translator = PigLatin("")
+        self.assertEqual(translator.get_phrase(), "nil")
